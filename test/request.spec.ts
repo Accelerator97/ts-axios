2a8fb6ca97d866eef8932daaa54600295935f6f8
xuebin <928691436@qq.com>
2022-01-26 15:00:20 +0800

    更新依赖

--- test/request.spec.ts
+++ test/request.spec.ts
@@ -6,302 +6,301 @@ import { getAjaxRequest } from './helper'
 //方式二：测试函数返回一个promise对象，返回一个resolve或者reject表示测试完毕
 
 describe('requests', () => {
-    beforeEach(() => {
-        jasmine.Ajax.install()
+  beforeEach(() => {
+    jasmine.Ajax.install()
+  })
+
+  afterEach(() => {
+    jasmine.Ajax.uninstall()
+  })
+
+  //request的method是大写
+  test('should treat single string arg as url', () => {
+    axios('/foo')
+    return getAjaxRequest().then(request => {
+      expect(request.url).toBe('/foo')
+      expect(request.method).toBe('GET')
+    })
+  })
+
+  //response配置中的method是小写
+  test('should treat method value as lowercase string', done => {
+    axios({
+      url: '/foo',
+      method: 'POST'
+    }).then(response => {
+      expect(response.config.method).toBe('post')
+      done()
+    })
+    //【注意】执行完respondWith 再执行axios.then方法中的expect判断
+    return getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 200
+      })
+    })
+  })
+
+  //这个会发出真正的axios请求，因为没有响应 会报connect ECONNREFUSED错误
+  test('should reject on network errors', done => {
+    const resolveSpy = jest.fn((res: AxiosResponse) => {
+      return res
     })
 
-    afterEach(() => {
-        jasmine.Ajax.uninstall()
+    const rejectSpy = jest.fn((e: AxiosError) => {
+      return e
     })
 
-    //request的method是大写
-    test('should treat single string arg as url', () => {
-        axios('/foo')
-        return getAjaxRequest().then(request => {
-            expect(request.url).toBe('/foo')
-            expect(request.method).toBe('GET')
-        })
+    jasmine.Ajax.uninstall()
 
+    axios('/foo')
+      .then(resolveSpy)
+      .catch(rejectSpy)
+      .then(next)
+
+    function next(reason: AxiosResponse | AxiosError) {
+      expect(resolveSpy).not.toHaveBeenCalled()
+      expect(rejectSpy).toHaveBeenCalled()
+      expect(reason instanceof Error).toBeTruthy()
+      expect((reason as AxiosError).message).toBe('Network Error')
+      expect(reason.request).toEqual(expect.any(XMLHttpRequest))
+
+      jasmine.Ajax.install()
+
+      done()
+    }
+  })
+
+  test('should reject when request timeout', done => {
+    let err: AxiosError
+
+    axios('/foo', {
+      timeout: 2000,
+      method: 'post'
+    }).catch(error => {
+      err = error
     })
 
-    //response配置中的method是小写
-    test('should treat method value as lowercase string', (done) => {
-        axios({
-            url: '/foo',
-            method: 'POST'
-        }).then(response => {
-            expect(response.config.method).toBe('post')
-            done()
-        })
-        //【注意】执行完respondWith 再执行axios.then方法中的expect判断
-        return getAjaxRequest().then(request => {
-            request.respondWith({
-                status: 200
-            })
-        })
+    getAjaxRequest().then(request => {
+      //由于 request.responseTimeout 方法内部依赖了 jasmine.clock 方法会导致运行失败，这里用了 request.eventBus.trigger('timeout') 方法触发了 timeout 事件。
+      //因为这个方法不在接口定义中，所以需要加  @ts-ignore。
+      // @ts-ignore
+      request.eventBus.trigger('timeout')
+      setTimeout(() => {
+        expect(err instanceof Error).toBeTruthy()
+        expect(err.message).toBe('Timeout of 2000 ms exceeded')
+        done()
+      }, 100)
+    })
+  })
+
+  //用户自定义合法状态码
+  test('should resolve when validateStatus returns true', done => {
+    const resolveSpy = jest.fn((res: AxiosResponse) => {
+      return res
     })
 
-    test('should reject on network errors', (done) => {
-        const resolveSpy = jest.fn((res: AxiosResponse) => {
-            return res
-        })
+    const rejectSpy = jest.fn((e: AxiosError) => {
+      return e
+    })
 
-        const rejectSpy = jest.fn((e: AxiosError) => {
-            return e
-        })
+    axios('/foo', {
+      validateStatus(status) {
+        return status === 500
+      }
+    })
+      .then(resolveSpy)
+      .catch(rejectSpy)
+      .then(next)
 
-        //卸载jasmine.Ajax 模拟网络错误
-        jasmine.Ajax.uninstall()
+    getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 500
+      })
+    })
 
-        axios('/foo')
-            .then(resolveSpy)
-            .catch(rejectSpy)
-            .then(next)
+    function next(res: AxiosResponse | AxiosError) {
+      expect(resolveSpy).toHaveBeenCalled()
+      expect(rejectSpy).not.toHaveBeenCalled()
+      expect(res.config.url).toBe('/foo')
+      done()
+    }
+  })
+  //用户自定义合法状态码
+  test('should reject when validateStatus returns false', done => {
+    const resolveSpy = jest.fn((res: AxiosResponse) => {
+      return res
+    })
 
-        function next(reason: AxiosResponse | AxiosError) {
-            expect(resolveSpy).not.toHaveBeenCalled()
-            expect(rejectSpy).toHaveBeenCalled()
-            expect(reason instanceof Error).toBeTruthy()
-            expect((reason as AxiosError).message).toBe('Network Error')
-            //expect.any(constructor) 它表示匹配任意由 constructor 创建的对象实例。
-            expect(reason.request).toEqual(expect.any(XMLHttpRequest))
-            jasmine.Ajax.install()
-            done()
+    const rejectSpy = jest.fn((e: AxiosError) => {
+      return e
+    })
+
+    axios('/foo', {
+      validateStatus(status) {
+        return status !== 500
+      }
+    })
+      .then(resolveSpy)
+      .catch(rejectSpy)
+      .then(next)
+
+    getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 500
+      })
+    })
+
+    function next(reason: AxiosError | AxiosResponse) {
+      expect(resolveSpy).not.toHaveBeenCalled()
+      expect(rejectSpy).toHaveBeenCalled()
+      expect(reason instanceof Error).toBeTruthy()
+      expect((reason as AxiosError).message).toBe('Request failed with status code 500')
+      expect((reason as AxiosError).response!.status).toBe(500)
+
+      done()
+    }
+  })
+  //正常情况下response返回数据是一个JSON对象，如果它是一个JSON字符串的话需要转为JSON对象
+  test('should return JSON when resolved', done => {
+    let response: AxiosResponse
+
+    axios('/api/account/signup', {
+      auth: {
+        username: '',
+        password: ''
+      },
+      method: 'post',
+      headers: {
+        Accept: 'application/json'
+      }
+    }).then(res => {
+      response = res
+    })
+
+    getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 200,
+        statusText: 'OK',
+        responseText: '{"a": 1}'
+      })
+
+      setTimeout(() => {
+        expect(response.data).toEqual({ a: 1 })
+        done()
+      }, 100)
+    })
+  })
+  //请求被拒绝时返回的数据时JSON对象
+  test('should return JSON when rejecting', done => {
+    let response: AxiosResponse
+
+    axios('/api/account/signup', {
+      auth: {
+        username: '',
+        password: ''
+      },
+      method: 'post',
+      headers: {
+        Accept: 'application/json'
+      }
+    }).catch(error => {
+      response = error.response
+    })
+
+    getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 400,
+        statusText: 'Bad Request',
+        responseText: '{"error": "BAD USERNAME", "code": 1}'
+      })
+
+      setTimeout(() => {
+        expect(typeof response.data).toBe('object')
+        expect(response.data.error).toBe('BAD USERNAME')
+        expect(response.data.code).toBe(1)
+        done()
+      }, 100)
+    })
+  })
+
+  test('should supply correct response', done => {
+    let response: AxiosResponse
+
+    axios.post('/foo').then(res => {
+      response = res
+    })
+
+    getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 200,
+        statusText: 'OK',
+        responseText: '{"foo": "bar"}',
+        responseHeaders: {
+          'Content-Type': 'application/json'
         }
+      })
+
+      setTimeout(() => {
+        expect(response.data.foo).toBe('bar')
+        expect(response.status).toBe(200)
+        expect(response.statusText).toBe('OK')
+        expect(response.headers['content-type']).toBe('application/json')
+        done()
+      }, 100)
     })
+  })
 
-    test('should reject when request timeout', (done) => {
-        let err: AxiosError
+  test('should allow overriding Content-Type header case-insensitive', () => {
+    let response: AxiosResponse
 
-        axios('/foo', {
-            timeout: 2000,
-            method: 'post'
-        }).catch(error => {
-            err = error
-        })
-
-        getAjaxRequest().then(request => {
-            //由于 request.responseTimeout 方法内部依赖了 jasmine.clock 方法会导致运行失败，这里用了 request.eventBus.trigger('timeout') 方法触发了 timeout 事件。
-            //因为这个方法不在接口定义中，所以需要加  @ts-ignore。
-            // @ts-ignore
-            request.eventBus.trigger('timeout')
-            setTimeout(() => {
-                expect(err instanceof Error).toBeTruthy()
-                expect(err.message).toBe('Timeout of 2000 ms exceeded')
-                done()
-            }, 100)
-        })
-    })
-
-    //用户自定义合法状态码
-    test('should resolve when validateStatus returns true', done => {
-        const resolveSpy = jest.fn((res: AxiosResponse) => {
-            return res
-        })
-
-        const rejectSpy = jest.fn((e: AxiosError) => {
-            return e
-        })
-
-        axios('/foo', {
-            validateStatus(status) {
-                return status === 500
-            }
-        })
-            .then(resolveSpy)
-            .catch(rejectSpy)
-            .then(next)
-
-        getAjaxRequest().then(request => {
-            request.respondWith({
-                status: 500
-            })
-        })
-
-        function next(res: AxiosResponse | AxiosError) {
-            expect(resolveSpy).toHaveBeenCalled()
-            expect(rejectSpy).not.toHaveBeenCalled()
-            expect(res.config.url).toBe('/foo')
-            done()
+    axios
+      .post(
+        '/foo',
+        { prop: 'value' },
+        {
+          headers: {
+            'content-type': 'application/json'
+          }
         }
+      )
+      .then(res => {
+        response = res
+      })
+
+    return getAjaxRequest().then(request => {
+      expect(request.requestHeaders['Content-Type']).toBe('application/json')
     })
-    //用户自定义合法状态码
-    test('should reject when validateStatus returns false', done => {
-        const resolveSpy = jest.fn((res: AxiosResponse) => {
-            return res
-        })
+  })
 
-        const rejectSpy = jest.fn((e: AxiosError) => {
-            return e
-        })
+  test('should support array buffer response', done => {
+    let response: AxiosResponse
 
-        axios('/foo', {
-            validateStatus(status) {
-                return status !== 500
-            }
-        })
-            .then(resolveSpy)
-            .catch(rejectSpy)
-            .then(next)
+    function str2ab(str: string) {
+      const buff = new ArrayBuffer(str.length * 2)
+      const view = new Uint16Array(buff)
+      for (let i = 0; i < str.length; i++) {
+        view[i] = str.charCodeAt(i)
+      }
+      return buff
+    }
 
-        getAjaxRequest().then(request => {
-            request.respondWith({
-                status: 500
-            })
-        })
-
-        function next(reason: AxiosError | AxiosResponse) {
-            expect(resolveSpy).not.toHaveBeenCalled()
-            expect(rejectSpy).toHaveBeenCalled()
-            expect(reason instanceof Error).toBeTruthy()
-            expect((reason as AxiosError).message).toBe('Request failed with status code 500')
-            expect((reason as AxiosError).response!.status).toBe(500)
-
-            done()
-        }
-    })
-    //正常情况下response返回数据是一个JSON对象，如果它是一个JSON字符串的话需要转为JSON对象
-    test('should return JSON when resolved', done => {
-        let response: AxiosResponse
-
-        axios('/api/account/signup', {
-            auth: {
-                username: '',
-                password: ''
-            },
-            method: 'post',
-            headers: {
-                Accept: 'application/json'
-            }
-        }).then(res => {
-            response = res
-        })
-
-        getAjaxRequest().then(request => {
-            request.respondWith({
-                status: 200,
-                statusText: 'OK',
-                responseText: '{"a": 1}'
-            })
-
-            setTimeout(() => {
-                expect(response.data).toEqual({ a: 1 })
-                done()
-            }, 100)
-        })
-    })
-    //请求被拒绝时返回的数据时JSON对象
-    test('should return JSON when rejecting', done => {
-        let response: AxiosResponse
-
-        axios('/api/account/signup', {
-            auth: {
-                username: '',
-                password: ''
-            },
-            method: 'post',
-            headers: {
-                Accept: 'application/json'
-            }
-        }).catch(error => {
-            response = error.response
-        })
-
-        getAjaxRequest().then(request => {
-            request.respondWith({
-                status: 400,
-                statusText: 'Bad Request',
-                responseText: '{"error": "BAD USERNAME", "code": 1}'
-            })
-
-            setTimeout(() => {
-                expect(typeof response.data).toBe('object')
-                expect(response.data.error).toBe('BAD USERNAME')
-                expect(response.data.code).toBe(1)
-                done()
-            }, 100)
-        })
+    axios('/foo', {
+      responseType: 'arraybuffer'
+    }).then(data => {
+      response = data
     })
 
-    test('should supply correct response', done => {
-        let response: AxiosResponse
+    getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 200,
+        // @ts-ignore
+        response: str2ab('Hello world')
+      })
 
-        axios.post('/foo').then(res => {
-            response = res
-        })
-
-        getAjaxRequest().then(request => {
-            request.respondWith({
-                status: 200,
-                statusText: 'OK',
-                responseText: '{"foo": "bar"}',
-                responseHeaders: {
-                    'Content-Type': 'application/json'
-                }
-            })
-
-            setTimeout(() => {
-                expect(response.data.foo).toBe('bar')
-                expect(response.status).toBe(200)
-                expect(response.statusText).toBe('OK')
-                expect(response.headers['content-type']).toBe('application/json')
-                done()
-            }, 100)
-        })
+      setTimeout(() => {
+        expect(response.data.byteLength).toBe(22)
+        done()
+      }, 100)
     })
-
-
-    test('should allow overriding Content-Type header case-insensitive', () => {
-        let response: AxiosResponse
-
-        axios
-            .post(
-                '/foo',
-                { prop: 'value' },
-                {
-                    headers: {
-                        'content-type': 'application/json'
-                    }
-                }
-            )
-            .then(res => {
-                response = res
-            })
-
-        return getAjaxRequest().then(request => {
-            expect(request.requestHeaders['Content-Type']).toBe('application/json')
-        })
-    })
-
-    test('should support array buffer response', done => {
-        let response: AxiosResponse
-
-        function str2ab(str: string) {
-            const buff = new ArrayBuffer(str.length * 2)
-            const view = new Uint16Array(buff)
-            for (let i = 0; i < str.length; i++) {
-                view[i] = str.charCodeAt(i)
-            }
-            return buff
-        }
-
-        axios('/foo', {
-            responseType: 'arraybuffer'
-        }).then(data => {
-            response = data
-        })
-
-        getAjaxRequest().then(request => {
-            request.respondWith({
-                status: 200,
-                // @ts-ignore
-                response: str2ab('Hello world')
-            })
-
-            setTimeout(() => {
-                expect(response.data.byteLength).toBe(22)
-                done()
-            }, 100)
-        })
-    })
+  })
 })
