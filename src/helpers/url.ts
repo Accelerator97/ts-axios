200e7463f78a9dfe6ff31aa230535bf4b7451d79
xuebin <928691436@qq.com>
2022-01-26 17:42:33 +0800

    [build]

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -16,7 +16,7 @@ function encode(val: string): string {
 }
 
 export function buildURL(url: string, params?: any, paramsSerializer?: (params: any) => string) {
-  //如果没传params 就不用进行url的拼接
+  // 如果没传params 就不用进行url的拼接
   if (!params) {
     return url
   }
@@ -35,34 +35,34 @@ export function buildURL(url: string, params?: any, paramsSerializer?: (params: 
       let values = []
       if (Array.isArray(val)) {
         values = val
-        key += '[]' //参数值为数组时  拼接后的url为/base/get?foo[]=bar&foo[]=baz ，所以key要+='[]'
+        key += '[]' // 参数值为数组时  拼接后的url为/base/get?foo[]=bar&foo[]=baz ，所以key要+='[]'
       } else {
-        values = [val] //把val放在数组里，这是为了调用forEach方法
+        values = [val] // 把val放在数组里，这是为了调用forEach方法
       }
       values.forEach(val => {
-        //遍历values的每一项val
+        // 遍历values的每一项val
         if (isDate(val)) {
-          //val如果是日期，调用toISOString进行拼接
+          // val如果是日期，调用toISOString进行拼接
           val = val.toISOString()
         } else if (isObject(val)) {
-          //val如果是对象，调用JSON.stringify继续拼接
+          // val如果是对象，调用JSON.stringify继续拼接
           val = JSON.stringify(val)
         }
         parts.push(`${encode(key)}=${encode(val)}`)
       })
     })
 
-    //parts的每一项通过&来连接
+    // parts的每一项通过&来连接
     serializeParams = parts.join('&')
   }
 
   if (serializeParams) {
-    //看是否有哈希标识符，有的话要忽略#及#后面的内容
+    // 看是否有哈希标识符，有的话要忽略#及#后面的内容
     const markIndex = url.indexOf('#')
     if (markIndex !== -1) {
       url = url.slice(0, markIndex)
     }
-    //如果前面没有? 需要拼上? 否则 拼上 &
+    // 如果前面没有? 需要拼上? 否则 拼上 &
     url += (url.indexOf('?') === -1 ? '?' : '&') + serializeParams
   }
   return url
@@ -90,7 +90,7 @@ interface URLOrigin {
 export function isURLSameOrigin(requestURL: string): boolean {
   let urlParsingNode = document.createElement('a')
 
-  // 1.获取当前页面地址的协议、域名、端口号
+  // 获取当前页面地址的协议、域名、端口号
   const currentOrigin = resolveURL(window.location.href)
   // 获取请求的url的协议、域名、端口号
   const parsedOrigin = resolveURL(requestURL)
